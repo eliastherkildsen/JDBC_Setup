ae953174918675750681cade305b62c7af70196d
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2023-11-02 11:54:56 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,95 @@
 import java.sql.*;
 import java.util.Properties;
 
-public class Main {
+public class Main{
 
-    // database name.
-    private static final String databaseName = "dbTest";
+    // JDBC PROPS
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "1234";
+    public static final String DATABASE_NAME = "dbChrono5";
+    public static final String PORT = "1433";
+    public static final String ENCRYPT = "false";
+    public static final String URL = "jdbc:sqlserver://localhost:"+ PORT +";databaseName="+DATABASE_NAME;
 
-    public static void main(String[] args) throws SQLException {
+    public static Connection connection = null;
 
-        // creating connection String
-        String url = "jdbc:sqlserver://localhost:1433;databaseName="+databaseName;
+    public static void main(String[] args) {
 
-        // creating the property's for JDBC connection.
+        System.out.printf("%sProgram starting.%s%n", ANSI_YELLOW, ANSI_RESET);
+
+        // getting props
+        Properties properties = setProps();
+        System.out.printf("%sSetting up props.%s%n", ANSI_YELLOW, ANSI_RESET);
+
+        // creating JDBC connection
+        connection = databaseConnection(properties, URL);
+        System.out.printf("%sCreating connection.%s%n", ANSI_YELLOW, ANSI_RESET);
+
+        // your code
+        
+
+        // closing JDBC connection
+        databaseClose(connection);
+
+
+    }
+
+    /***
+     * Properties method used to create a properties containing all props used to
+     * connect to JDBC
+     * @return properties
+     */
+    public static Properties setProps(){
+        // creating an intestines of properties.
         Properties properties = new Properties();
+        properties.setProperty("user", USERNAME);
+        properties.setProperty("password", PASSWORD);
+        properties.setProperty("encrypt", ENCRYPT);
 
-        // Creating user parameters
-        properties.setProperty("user", "sa");
-        properties.setProperty("password", "1234");
+        databaseConnection(properties, URL);
+        return properties;
+    }
 
-        // set the SSL property
-        properties.setProperty("encrypt", "false");
-
-        // create connection.
-        Connection connection = DriverManager.getConnection(url, properties);
-
-
-        System.out.println("Hello JDBC");
-
-        // Updating prepaired statments.
-        //PreparedStatement preparedStatement = connection.prepareCall("INSERT INTO tblUser (fldID, fldName) VALUES (6, 'elias')");
-        PreparedStatement preparedStatement = connection.prepareCall("SELECT * FROM tblUser");
-
-        // setting up result set
-        //ResultSet resultSet = preparedStatement.executeQuery(); // Read type.
-        //preparedStatement.executeUpdate();
-
-
+    /***
+     * Method used to close a database connection.
+     * @param connection
+     */
+    public static void databaseClose(Connection connection){
         try {
-            // Assuming you already have a PreparedStatement object named preparedStatement
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            while (resultSet.next()) {
-                // Assuming "tblUser" has columns like "column1", "column2", etc.
-                int column1Value = resultSet.getInt("fldID");
-                String column2Value = resultSet.getString("fldName");
-                // Retrieve other columns as needed
-
-                System.out.println("column1: " + column1Value + ", column2: " + column2Value);
-                // Print other columns as needed
-            }
-
-            resultSet.close();
+            connection.close();
         } catch (SQLException e) {
-
-
+            throw new RuntimeException(e);
         }
 
+        // closing connection to JDBC
+        System.out.printf("%sClosing connection to JDBC..%s", ANSI_YELLOW, ANSI_RESET);
 
 
 
     }
 
-}
+    /***
+     * method for creating a connection to a database
+     * @param properties
+     * @param URL
+     * @return
+     */
+    public static Connection databaseConnection(Properties properties, String URL){
+
+        // initializes connection.
+        Connection connection = null;
+
+        // JDBC tries to connect to SQL database add URL.
+        try {
+            connection = DriverManager.getConnection(URL, properties);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return connection;
+
+    }
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
