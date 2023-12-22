8f896bfcc5bcd24314a399315285ade47dfafdd7
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2023-12-22 18:02:25 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,42 @@ import java.util.Properties;
 
 public class Main {
 
-    public static Connection connection = null;
+    public static JDBC db = new JDBC();
+    public static Connection connection = db.getConnection();
+
+    public static void testDB(){
+        PreparedStatement getProject;
+        try {
+            getProject = connection.prepareCall("SELECT * FROM tblProject where fldProjectID=" + 1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+
+            ResultSet projectDetails = getProject.executeQuery();
+            while (projectDetails.next()) {
+
+                String projectId = projectDetails.getString("fldProjectID");
+                String projectName = projectDetails.getString("fldProjectName");
+                String projectStart = projectDetails.getString("fldProjectStartDate");
+                String projectEnd = projectDetails.getString("fldProjectEndDate");
+
+                System.out.printf("%s%s%n", AnsiColorCode.ANSI_GREEN,"Project ID: " + projectId);
+                System.out.printf("%s%n","Project name: " + projectName);
+                System.out.printf("%s%n","Start date: " + projectStart);
+                System.out.printf("%s%s%n","End date: " + projectEnd,AnsiColorCode.ANSI_RESET);
+            }
+        } catch (SQLException ignore) {}
+    }
+
 
     public static void main(String[] args) {
 
-        // getting props, and setting up props.
-        Properties properties = JDBC_Setup.setProps();
+        public static JDBC db = new JDBC();
+        public static Connection connection = db.getConnection();
+        testDB(); 
+        }
 
-        // creating JDBC connection
-        connection = JDBC_Setup.databaseConnection(properties, JDBC_Setup.URL);
-
-
-        // your code
-
-        
-        // closing JDBC connection
-        JDBC_Setup.databaseClose(connection);
-
-
-
-    }
+    
 }
+
